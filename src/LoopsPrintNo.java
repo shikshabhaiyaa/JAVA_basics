aff73dd4bc63db6607f58da7b729ed75acf6bad9
Shikha Chaurasiya <chaurasiyashikha91@gmail.com>
2026-09-17 20:57:21 +0530

    Added DSA questions

--- src/LoopsPrintNo.java
+++ src/LoopsPrintNo.java
@@ -12,6 +12,5 @@
         for(int i = 1; i <= n ; i++){
             System.out.println(i);
         }
-
     }
 }
--- src/LoopsSumOfNo.java
+++ src/LoopsSumOfNo.java
@@ -12,7 +12,5 @@ public class LoopsSumOfNo {
             sum = sum + i;
         }
         System.out.println(sum);
-
-
     }
 }
